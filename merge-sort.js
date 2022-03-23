46eec6b918b9384185be3b5948cc3c4f79dd95d5
Chris Young <ctopheryoung@gmail.com>
2022-03-23 15:16:27 -0700

    merge sort

---
+++ merge-sort.js
@@ -0,0 +1,30 @@
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr
+  }
+
+  const middle = Math.floor(arr.length / 2)
+  const left = arr.slice(0, middle)
+  const right = arr.slice(middle)
+
+  return merge(mergeSort(left), mergeSort(right))
+}
+
+function merge(left, right) {
+  const sorted = []
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      sorted.push(left.shift())
+    }
+    else {
+      sorted.push(right.shift())
+    }
+  }
+
+  return [...sorted, ...left, ...right]
+}
+
+// console.log(mergeSort([10, 9, 8, 7, 6]))
+// console.log(mergeSort([4, 1, 9, 0]))
+console.log(mergeSort([2, 1]))
